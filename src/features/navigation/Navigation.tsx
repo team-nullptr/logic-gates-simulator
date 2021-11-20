cf62eca7cd6c49cb9c8ef0a4e0bdd99873ca4247
bk20dev <60577942+bk20dev@users.noreply.github.com>
2021-11-20 23:31:48 +0100

    Style navigation

--- src/features/navigation/Navigation.tsx
+++ src/features/navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { NavigationItem } from "../../common/models/NavigationItem";
-import { Home } from "react-feather";
+import { Home, Icon } from "react-feather";
+import styles from "./Navigation.module.scss";
 
 type NavigationProps = {
   title: string;
@@ -8,27 +9,32 @@ type NavigationProps = {
   actions: NavigationItem[];
 };
 
+const NavigationIcon: FC<{ icon: Icon; text?: string; onClick: () => void }> = (
+  props
+) => (
+  <button className={`${styles.button} ${props.text ? styles.expanded : ""}`}>
+    <props.icon className={styles.icon} onClick={props.onClick} />
+    {props.text && <span className={styles.text}>{props.text}</span>}
+  </button>
+);
+
 export const Navigation: FC<NavigationProps> = (props) => {
   const renderActions = () => {
-    return props.actions.map((action, i) => {
-      if (!action.text) {
-        return <action.icon onClick={action.onClick} key={i} />;
-      }
-
-      return (
-        <button onClick={action.onClick} key={i}>
-          {<action.icon />}
-          {action.text}
-        </button>
-      );
-    });
+    return props.actions.map((action, i) => (
+      <NavigationIcon
+        icon={action.icon}
+        onClick={action.onClick}
+        text={action.text}
+        key={i}
+      />
+    ));
   };
 
   return (
-    <nav>
-      <Home onClick={props.onHomeClicked} />
-      <p>{props.title}</p>
-      <div>{renderActions()}</div>
+    <nav className={styles.navigation}>
+      <NavigationIcon icon={Home} onClick={props.onHomeClicked} />
+      <p className={styles.title}>{props.title}</p>
+      <div className={styles.actions}>{renderActions()}</div>
     </nav>
   );
 };
