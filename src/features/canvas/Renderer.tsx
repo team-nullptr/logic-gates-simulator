655a128a0fbab8f250cdb87b89e79e6bc7f1647d
bk20dev <60577942+bk20dev@users.noreply.github.com>
2022-01-01 16:38:03 +0100

    Create render method Register resize event handler

--- src/features/canvas/Renderer.tsx
+++ src/features/canvas/Renderer.tsx
@@ -1,6 +1,7 @@
 import { Vector } from "./types/Vector";
 
 export class Renderer {
+  private running = true;
   private readonly ctx: CanvasRenderingContext2D;
 
   private dragging = false;
@@ -10,18 +11,32 @@ export class Renderer {
   constructor(private readonly canvas: HTMLCanvasElement) {
     this.ctx = canvas.getContext("2d")!;
     this.initialize();
+    this.render();
   }
 
   destroy() {
     this.canvas.removeEventListener("mousedown", this.handleMouseDown);
     this.canvas.removeEventListener("mousemove", this.handleMouseMove);
     removeEventListener("mouseup", this.handleMouseUp);
+    removeEventListener("resize", this.resize);
+    this.running = false;
   }
+
+  private render = () => {
+    if (!this.running) return;
+
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.setTransform(1, 0, 0, 1, ...this.offset);
+
+    requestAnimationFrame(this.render);
+  };
 
   private initialize() {
     this.canvas.addEventListener("mousedown", this.handleMouseDown);
     this.canvas.addEventListener("mousemove", this.handleMouseMove);
     addEventListener("mouseup", this.handleMouseUp);
+    addEventListener("resize", this.resize);
+    this.resize();
   }
 
   private handleMouseDown = ({ offsetX, offsetY }: MouseEvent) => {
@@ -42,5 +57,10 @@ export class Renderer {
 
   private handleMouseUp = () => {
     this.dragging = false;
+  };
+
+  private resize = () => {
+    this.canvas.width = this.canvas.offsetWidth;
+    this.canvas.height = this.canvas.offsetHeight;
   };
 }
