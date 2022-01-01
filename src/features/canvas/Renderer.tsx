6f5ec21a29a31302260f4858de5584ece6e4ab81
bk20dev <60577942+bk20dev@users.noreply.github.com>
2022-01-01 15:09:20 +0100

    Register event listeners

--- src/features/canvas/Renderer.tsx
+++ src/features/canvas/Renderer.tsx
@@ -1,9 +1,32 @@
 export class Renderer {
-  private ctx: CanvasRenderingContext2D;
+  private readonly ctx: CanvasRenderingContext2D;
 
-  constructor(canvas: HTMLCanvasElement) {
+  constructor(private readonly canvas: HTMLCanvasElement) {
     this.ctx = canvas.getContext("2d")!;
+    this.initialize();
   }
 
-  destroy() {}
+  destroy() {
+    this.canvas.removeEventListener("mousedown", this.handleMouseDown);
+    this.canvas.removeEventListener("mousemove", this.handleMouseMove);
+    removeEventListener("mouseup", this.handleMouseUp);
+  }
+
+  private initialize() {
+    this.canvas.addEventListener("mousedown", this.handleMouseDown);
+    this.canvas.addEventListener("mousemove", this.handleMouseMove);
+    addEventListener("mouseup", this.handleMouseUp);
+  }
+
+  private handleMouseDown = (e: MouseEvent) => {
+    console.log("mousedown", e);
+  };
+
+  private handleMouseMove = (e: MouseEvent) => {
+    console.log("mousemove", e);
+  };
+
+  private handleMouseUp = (e: MouseEvent) => {
+    console.log("mouseup", e);
+  };
 }
