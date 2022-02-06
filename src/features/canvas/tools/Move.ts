3f9294a9f7c774ec36dab9897f6bd19add6b5459
bk20dev <itsbk20@gmail.com>
2022-02-06 21:28:17 +0100

    Create Move Tool

---
+++ src/features/canvas/tools/Move.ts
@@ -0,0 +1,28 @@
+import { Tool } from "./Tool";
+import { Interaction } from "../types/Interaction";
+import { Vector } from "../../../common/Vector";
+import { Block } from "../../../common/Block";
+import { subtract } from "../../../common/utils";
+
+const snapToGrid = (position: Vector): Vector => {
+  return position.map((it) => Math.round(it / 48)) as Vector;
+};
+
+export class MoveTool extends Tool {
+  private offset!: Vector;
+  private target!: Block;
+
+  handleMouseDown(interaction: Interaction) {
+    const { position, target } = interaction;
+    const block = target as Block;
+
+    const corner = block.area.slice(0, 2) as Vector;
+    this.offset = subtract(position, corner);
+    this.target = block;
+  }
+
+  handleMouseMove({ position, target }: Interaction) {
+    const corner = subtract(position, this.offset);
+    this.target.position = snapToGrid(corner);
+  }
+}
--- src/features/canvas/tools/ToolFactory.ts
+++ src/features/canvas/tools/ToolFactory.ts
@@ -1,12 +1,13 @@
 import { Tool } from "./Tool";
 import { PanTool } from "./Pan";
+import { MoveTool } from "./Move";
 
 export type ToolType = "pan" | "move" | "connect";
 
 export class ToolFactory {
   private static readonly tools: { [k: string]: Tool } = {
     pan: new PanTool(),
-    move: new (class extends Tool {})(),
+    move: new MoveTool(),
     connect: new (class extends Tool {})(),
   };
 
