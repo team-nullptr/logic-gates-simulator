0a02b98ee75c0438a70579f5f499bc364d244dcb
SeNicko <sebastianflajszer@gmail.com>
2022-02-08 19:45:16 +0100

    Update example test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
 
-test('jest is working fine!', () => {
+test('jest', () => {
   render(<a>hello</a>);
   expect(screen.getByText('hello')).toHaveTextContent('hello');
+});
+
+test('is', () => {
+  expect(1).toBe(1);
+});
+
+test('working', () => {
+  expect(true).toBe(true);
 });
