58206da1a289d56ee50ae9e2bfc6876903a18f8d
SeNicko <sebastianflajszer@gmail.com>
2022-02-09 19:16:58 +0100

    Write tests for BaseGate and fetchSavedGates util function

---
+++ src/core/simulator/elements/__test__/BaseGate.test.ts
@@ -0,0 +1,37 @@
+import {
+  BaseGate,
+  BaseGateOptions,
+  BaseGateType,
+  gatesOptions
+} from '../BaseGate';
+
+describe('Base gates are setup correctly', () => {
+  test.each(['not', 'or', 'and', 'nand'] as BaseGateType[])(
+    'Base gate %s is defined',
+    (type) => {
+      const gate = gatesOptions.get(type);
+      expect(gate).toBeDefined();
+    }
+  );
+});
+
+test('Creates base gate correctly', () => {
+  const id = '1';
+  const handlerOutput = true;
+  const options: BaseGateOptions = {
+    type: 'test',
+    color: '#000',
+    inputsCount: 3,
+    handler: () => handlerOutput
+  };
+
+  const gate = new BaseGate(id, options);
+  gate.run();
+
+  expect(gate.id).toEqual(id);
+  expect(gate.connections).toEqual([]);
+  expect(gate.states[0]).toEqual(handlerOutput);
+  expect(gate.color).toEqual(options.color);
+  expect(gate.inputs.length).toEqual(options.inputsCount);
+  expect(gate.type).toEqual(options.type);
+});
