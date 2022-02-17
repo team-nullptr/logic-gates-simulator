ae83a0f2463e2235148f387a30e338c6430333fb
SeNicko <sebastianflajszer@gmail.com>
2022-02-17 15:17:20 +0100

    Make project card a link to it's edit page

--- src/features/dashboard/DashboardCard.tsx
+++ src/features/dashboard/DashboardCard.tsx
@@ -2,19 +2,22 @@ import styles from './DashboardCard.module.scss';
 import blueprintImage from '../../assets/images/blueprint.png';
 import { Project } from '../../core/project-manager/ProjectManager';
 import { formatRelative } from 'date-fns';
+import { Link } from 'react-router-dom';
 
 interface CardProps {
   project: Project;
 }
 
-export const DashboardCard = ({ project: { name, modifiedAt } }: CardProps) => {
+export const DashboardCard = ({ project: { id, name, modifiedAt } }: CardProps) => {
   return (
-    <article className={styles.card}>
-      <img className={styles.card__image} src={blueprintImage} alt="blueprint" />
-      <div className={styles.card__meta}>
-        <p className={styles.card__title}>{name}</p>
-        <span className={styles.card__modified}>{formatRelative(modifiedAt, new Date())}</span>
-      </div>
-    </article>
+    <Link to={`/edit/${id}`}>
+      <article className={styles.card}>
+        <img className={styles.card__image} src={blueprintImage} alt="blueprint" />
+        <div className={styles.card__meta}>
+          <p className={styles.card__title}>{name}</p>
+          <span className={styles.card__modified}>{formatRelative(modifiedAt, new Date())}</span>
+        </div>
+      </article>
+    </Link>
   );
 };
