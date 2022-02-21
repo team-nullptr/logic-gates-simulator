c3a66f3ba70d251f6a01098fe44d82477b8fb90e
bk20dev <itsbk20@gmail.com>
2022-02-21 12:04:12 +0100

    Fix MessageBusCard and MessageBusRenderer styles

--- src/features/message-bus/MessageBusCard.tsx
+++ src/features/message-bus/MessageBusCard.tsx
@@ -1,36 +1,18 @@
 import styles from './MessageBusCard.module.scss';
-import { Message, MessageType } from './MessageBus';
-import { Check, AlertCircle } from 'react-feather';
+import { Message } from './MessageBus';
+import { Check, X } from 'react-feather';
 
 interface MessageBusCardProps {
   message: Message;
 }
 
-const getBoxStyle = (type: MessageType) => {
-  switch (type) {
-    case 'success':
-      return styles.card;
-    case 'error':
-      return styles.cardError;
-  }
-};
-
-const getBoxIcon = (type: MessageType) => {
-  switch (type) {
-    case 'success':
-      return <Check />;
-    case 'error':
-      return <AlertCircle />;
-  }
-};
-
 export const MessageBusCard = ({ message: { type, body } }: MessageBusCardProps) => {
+  const classes = [styles.card, styles[type]];
+
   return (
-    <div className={getBoxStyle(type)}>
-      <div>{getBoxIcon(type)}</div>
-      <div>
-        <p>{body}</p>
-      </div>
+    <div className={classes.join(' ')}>
+      <div className={styles.icon}>{type === 'success' ? <Check /> : <X />}</div>
+      <p className={styles.text}>{body}</p>
     </div>
   );
 };
